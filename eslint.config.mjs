22c3035fd20afa09fd7fd7b8b5b80a542fdebcac
Luiz Silva <luizsv.dev@gmail.com>
2025-05-31 15:20:50 -0300

    Add initial project linting config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,28 @@
-// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+    },
+  },
+  files: ['**/*.vue', '**/*.ts', '**/*.tsx', '**/*.mjs'],
+  rules: {
+    'no-console': 'warn',
+    'quotes': ['warn', 'single'],
+    'max-len': ['warn', 120],
+    'array-element-newline': ['warn', { multiline: true, minItems: 4 }],
+    'function-paren-newline': ['warn', 'multiline-arguments'],
+    'newline-per-chained-call': ['warn', { ignoreChainWithDepth: 3 }],
+    'semi': ['warn', 'always'],
+    'comma-dangle': ['warn', 'never'],
+    // @typescript-eslint
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/prefer-readonly': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    // Indent TS and Vue
+    'indent': ['warn', 'tab'],
+    'vue/html-indent': ['warn', 'tab'],
+    'vue/script-indent': ['warn', 'tab'],
+  },
+})
